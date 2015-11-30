87633e9d3f1857613b2886c2d386965d424b0caf
MyDeveloperDay <mydeveloperday@gmail.com>
2015-11-30 22:23:44 +0000

    Add google test to html example

---
+++ src/examples/GTest2Html/main.cxx
@@ -0,0 +1,41 @@
+#include "System.h"
+#include "System.IO.h"
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        public:
+        static void Main(Array<String> args)
+        {
+           if (args.Length() != 2)
+           {
+                System::Console::WriteLine(
+                        "Usage: GTest2Html <intput.txt> <output.html>");
+                       return;
+           }
+           StreamWriter sw(args[1]);
+           {
+               sw.WriteLine("<html>");
+               sw.WriteLine("<body>");
+               StreamReader sr(args[0]);
+               {
+                    while (!sr.EndOfStream())
+                    {
+                        String s = sr.ReadLine();
+                        System::Console::WriteLine(s);
+                        sw.WriteLine(s);
+                        sw.WriteLine("<br>");
+                    }
+                    sr.Dispose();
+               }
+               sw.WriteLine("</body>");
+               sw.WriteLine("</html>");
+               sw.Dispose();
+           }
+        }
+    };
+}
+
+
+

01f07f2da01f7dac415894831dd16e016e9b97f6
MyDeveloperDay <mydeveloperday@gmail.com>
2015-12-01 23:16:27 +0000

    new tests and travis file

---
+++ src/unittests/DateTime_Test.cxx
@@ -0,0 +1,16 @@
+
+#include "gtest/gtest.h"
+
+#include "framework/System.h"
+
+using namespace System;
+
+TEST(DateTimeTest, BasicDateOperations)
+{
+    DateTime time(2015,12,25);
+
+    EXPECT_EQ(25,time.Day());
+    EXPECT_EQ(12, time.Month());
+    EXPECT_EQ(2015, time.Year());
+}
+
---
+++ src/unittests/FileInfo_Test.cxx
@@ -0,0 +1,22 @@
+
+#include "gtest/gtest.h"
+
+#include "framework/System.h"
+#include "framework/System.IO.h"
+
+using namespace System;
+
+TEST(FileInfoTest, BasicFileOperations)
+{
+    FileInfo info("myfile.txt");
+
+    EXPECT_FALSE(info.Exists());
+
+    EXPECT_TRUE(info.FullName().Contains("myfile.txt"));
+
+    EXPECT_EQ(info.Name(),String("myfile.txt"));
+    EXPECT_TRUE(info.Name().Equals("myfile.txt"));
+
+    EXPECT_TRUE(info.Extension().Equals(".txt"));
+}
+

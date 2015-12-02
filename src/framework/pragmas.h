27a8b2dd41f72d9ed2957c5d824a8bdfba7c55b7
MyDeveloperDay <mydeveloperday@gmail.com>
2015-12-02 08:55:23 +0000

    REFACTOR    -  Move DateTime out into its own file    -  Move pragmas to a central file so we can control easily

---
+++ src/framework/pragmas.h
@@ -0,0 +1,7 @@
+// these pragama turn off what C# doesn't normally complain about
+#pragma warning(disable: 4100)
+#pragma warning(disable: 4505)
+
+// localtime TODO replace with secure call
+#pragma warning(disable: 4996)
+

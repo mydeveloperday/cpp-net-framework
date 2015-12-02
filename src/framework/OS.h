22dfba90fdda0572f76ca53ac300d1c5344177d8
MyDeveloperDay <mydeveloperday@gmail.com>
2015-12-02 15:16:20 +0000

    [test] fix Apple nulltpr warnings

--- src/framework/OS.h
+++ src/framework/OS.h
@@ -2,10 +2,21 @@
 #include "sys/stat.h"
 
 #ifdef _WIN32
+
 #include <direct.h>
 #define PATH_SEPARATOR "\\"
+#define SUPPORTS_NULLPTR
+
+#elif defined(_APPLE_)
+
+#include "unistd.h"
+#define SUPPORTS_NULLPTR
+#define PATH_SEPARATOR "/"
+
 #else
+
 #include "unistd.h"
 #include "NullPtr.h"
 #define PATH_SEPARATOR "/"
+
 #endif

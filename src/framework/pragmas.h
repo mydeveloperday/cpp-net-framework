6310e0565a45ebdefff652023b51bec6b2c359ed
MyDeveloperDay <mydeveloperday@gmail.com>
2015-12-02 15:18:12 +0000

    [test] fix Apple nulltpr warnings

--- src/framework/pragmas.h
+++ src/framework/pragmas.h
@@ -1,6 +1,11 @@
+
+#if defined(_WIN32)
+
 // these pragama turn off what C# doesn't normally complain about
 #pragma warning(disable : 4100)
 #pragma warning(disable : 4505)
 
 // localtime TODO replace with secure call
 #pragma warning(disable : 4996)
+
+#endirf

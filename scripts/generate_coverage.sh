b6526dd84b700e97fa7b396dea94fc239c9d0a4b
MyDeveloperDay <mydeveloperday@gmail.com>
2015-12-04 09:55:16 +0000

    [tool] adding coverage analysis

---
+++ scripts/generate_coverage.sh
@@ -0,0 +1,61 @@
+#!/bin/sh
+basepath=`pwd`
+
+function print_help {
+  echo "#######################################################################"
+  echo "###               Generation of Test Coverage Report                ###"
+  echo "#                                                                     #"
+  echo "# First (and only) parameter must be the name of the build directory  #"
+  echo "#                                                                     #"
+  echo "# Example:                                                            #"
+  echo "#   ./generate_coverage.sh build_gcc                                  #"
+  echo "#                                                                     #"
+  echo "#######################################################################"
+  return 0
+}
+
+if [[ $# -ne 1 ]]
+then
+  print_help
+  echo "ERROR: Please name the build directory as the first parameter."
+  exit -1
+fi
+
+builddir=${1}
+cd ${builddir}
+
+rm -rf ${basepath}/coverage
+mkdir -p ${basepath}/coverage
+
+for testdir in `find ${basepath}/${builddir} -type d | grep -o 'tests/.*/.*\dir'`
+do
+  testname=`expr "$testdir" : '.*\(test_[a-zA-Z\-_]*\)\.dir'`
+  echo "Gathering Coverage for ${testname}"
+  cd $testdir
+  lcov --zerocounters  --directory .
+  cd ${basepath}/${builddir}
+  ctest -R $testname
+  cd $testdir
+  lcov --directory . --capture --output-file ${testname}.info.tmp
+  lcov --extract ${testname}.info.tmp "*${basepath}/include/**/*" \
+    --output-file ${testname}.info
+  rm ${testname}.info.tmp
+  cd ${basepath}/${builddir}
+  if [[ -e all_tests.info ]]
+  then
+    lcov --add-tracefile all_tests.info \
+      --add-tracefile ${testdir}/${testname}.info \
+      --output-file all_tests.info
+  else
+    lcov --add-tracefile ${testdir}/${testname}.info \
+      --output-file all_tests.info
+  fi
+done
+
+cd ${basepath}
+genhtml --output-directory ./coverage \
+  --demangle-cpp --num-spaces 2 --sort \
+  --title "My Program Test Coverage" --prefix ${basepath}/include \
+  --function-coverage --branch-coverage --legend \
+  ${basepath}/${builddir}/all_tests.info
+

42d85050a08341e58e6fad283b9504fbc0500202
MyDeveloperDay <mydeveloperday@gmail.com>
2015-12-04 23:51:06 +0000

    [tool] improve the unit test coverage

--- src/unittests/Math_Test.cxx
+++ src/unittests/Math_Test.cxx
@@ -9,7 +9,9 @@ using namespace System;
 TEST(MathTest, MaxMin)
 {
     EXPECT_EQ(2, Math::Max(1,2));
+    EXPECT_EQ(2, Math::Max(2,1));
     EXPECT_EQ(1, Math::Min(1,2));
+    EXPECT_EQ(1, Math::Min(2,1));
 
     EXPECT_EQ(2, Math::Max(-2,2));
     EXPECT_EQ(-2, Math::Min(-2,2));
@@ -18,7 +20,9 @@ TEST(MathTest, MaxMin)
     EXPECT_EQ(1, Math::Max(1,1));
 
     EXPECT_EQ(2.0, Math::Max(1.0,2.0));
+    EXPECT_EQ(2.0, Math::Max(2.0,1.0));
     EXPECT_EQ(1.0, Math::Min(1.0,2.0));
+    EXPECT_EQ(1.0, Math::Min(2.0,1.0));
 
     EXPECT_EQ(2.0, Math::Max(-2.0,2.0));
     EXPECT_EQ(-2.0, Math::Min(-2.0,2.0));

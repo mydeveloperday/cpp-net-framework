88f77d68f76a926cc1125223a2e0ab3d4482f093
MyDeveloperDay <mydeveloperday@gmail.com>
2015-12-04 04:43:19 +0000

    [tool] fix coverity scan errors

--- src/examples/GTest2Html/main.cxx
+++ src/examples/GTest2Html/main.cxx
@@ -1,3 +1,48 @@
+<<<<<<< HEAD
+#include "System.h"
+#include "System.IO.h"
+#include "System.Collections.Generic.h"
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+    public:
+        static void Main(Array<String> args)
+        {
+            if (args.Length() != 2) {
+                System::Console::WriteLine(
+                    "Usage: GTest2Html <intput.txt> <output.html>");
+                return;
+            }
+            try {
+                StreamWriter sw(args[1]);
+                {
+                    sw.WriteLine("<html>");
+                    sw.WriteLine("<body>");
+                    StreamReader sr(args[0]);
+                    {
+                        while (!sr.EndOfStream()) {
+                            String s = sr.ReadLine();
+                            System::Console::WriteLine(s);
+                            sw.WriteLine(s);
+                            sw.WriteLine("<br>");
+                        }
+                        sr.Dispose();
+                    }
+                    sw.WriteLine("</body>");
+                    sw.WriteLine("</html>");
+                    sw.Dispose();
+                }
+            }
+            catch (const System::Exception &e)
+            {
+                System::Console::WriteLine(e.Message);
+            }
+        }
+    };
+}
+=======
 #include "System.h"
 #include "System.IO.h"
 #include "System.Collections.Generic.h"
@@ -35,3 +80,4 @@ namespace ConsoleApplication1
         }
     };
 }
+>>>>>>> refs/remotes/origin/coverity_scan

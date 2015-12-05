3df7a5364a1829e66ab16dbad606a38e4c04689b
MyDeveloperDay <mydeveloperday@gmail.com>
2015-12-05 22:29:01 +0000

    [tool] move the coverage to scripts to allow it to be turned off on other platforms

--- scripts/install_coverage.sh
+++ scripts/install_coverage.sh
@@ -2,13 +2,18 @@
 
 echo "Running Install Coverage on " ${TRAVIS_OS_NAME}
 
-echo "Installing Coverage Tools"
-set -ev
-cd ${TRAVIS_BUILD_DIR}
-wget http://ftp.de.debian.org/debian/pool/main/l/lcov/lcov_1.11.orig.tar.gz
-tar xf lcov_1.11.orig.tar.gz
-sudo make -C lcov-1.11/ install
-gem install coveralls-lcov
-lcov --version
-g++ --version
-lcov --directory . --zerocounters
+if [[ ${TRAVIS_OK_NAME} = "linux" ]]
+then
+   echo "Installing Coverage Tools"
+   set -ev
+   cd ${TRAVIS_BUILD_DIR}
+   wget http://ftp.de.debian.org/debian/pool/main/l/lcov/lcov_1.11.orig.tar.gz
+   tar xf lcov_1.11.orig.tar.gz
+   sudo make -C lcov-1.11/ install
+   gem install coveralls-lcov
+   lcov --version
+   g++ --version
+   lcov --directory . --zerocounters
+else
+   echo "Ignoring Coverage On Mac"
+fi
